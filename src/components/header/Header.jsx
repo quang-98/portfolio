684c9540d4e4786345cdac3d568862e50ba8016d
quang-98 <81545697+quang-98@users.noreply.github.com>
2022-08-05 00:18:44 +0700

    commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import CTA from "./CTA";
 import "./Header.css";
+import ME from "../../assets/me.png";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm </h5>
+        <h1>Quang Võ</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
